d042322a2e2a0659e212379744326b3ff486db19
DebyGrey <demifashoro@gmail.com>
2023-08-04 17:35:34 +0100

    Add person, student, and teacher classes

--- student.rb
+++ student.rb
@@ -1,6 +1,6 @@
 class Student < Person
   def initialize(classroom, age, name: 'unknown', parent_permission: true)
-    super(age, name:, parent_permission:,)
+    super(age, name, parent_permission,)
     @classroom = classroom
   end
 
