ddfc75675392d3a9e4c95425b7c209512d9dc76e
DebyGrey <demifashoro@gmail.com>
2023-08-04 17:26:53 +0100

    Add person, student, and teacher classes

--- student.rb
+++ student.rb
@@ -1,10 +1,10 @@
 class Student < Person
-    def initialize(age, name = "unknown", parent_permission = true, classroom)
-        super(age, name: name, parent_permission: parent_permission,)
-        @classroom = classroom
-    end
+  def initialize(classroom, age, name = 'unknown', parent_permission: true)
+    super(age, name:, parent_permission:,)
+    @classroom = classroom
+  end
 
-    def play_hooky
-    "¯\\(ツ)/¯"
-    end
+  def play_hooky
+    '¯\\(ツ)/¯'
+  end
 end
